d766c09d2748897fb1c89819684846bdbab6079b
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-01-25 15:08:58 +0000

    saved base.html as base.php

---
+++ base.php
@@ -0,0 +1,57 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Home</title>
+    
+    <script src="js/base.js"></script>
+</head>
+    <body>
+        
+        <div class="header"> 
+
+            <div class="site-identity">
+                <a href="#"><img style="padding: 0px 5px 0px 0px;" src=images/Logo.png 
+                    width="100"
+                    height="100"
+                    alt="Portoflio" /></a>
+            </div>
+
+            <h1>Melony Lady : Sydney Humayun</h1>
+
+            <nav class="topnav">
+            
+                <a href="content.html" class="nav_link">
+                    Content
+                </a>
+    
+                <a href="cv.html" class="nav_link">
+                    CV
+                </a>
+    
+                <a href="portfolio.html" class="nav_link">
+                    Portfolio
+                </a> 
+    
+                <a href="index.html" class="nav_link">
+                    Home
+                </a>
+    
+            </nav>
+        </div>
+
+        <script src="app.js"></script>
+        <noscript>Please enable JavaScript to view the full site.</noscript>
+
+        
+    </body>
+</html>
