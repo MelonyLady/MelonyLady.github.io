07c390d448cd94b29ad8c11fb92a7cdf7e93b73a
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-01-25 16:14:42 +0000

    creation of php files from html

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MelonyLady: Sydney Humayun</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="js/app.js"></script>
+</head>
+    <body>
+        
+        <div class="header"> 
+
+            <div class="site-identity">
+                <a href="#"><img style="padding: 0px 5px 0px 0px;" src=images/Logo.png 
+                    width="100"
+                    height="100"
+                    alt="Portoflio" /></a>
+            </div>
+
+            <h1>Melony Lady : Sydney Humayun</h1>
+
+            <nav class="topnav">
+            
+                <a href="content.html" class="nav_link">
+                    Content
+                </a>
+    
+                <a href="cv.html" class="nav_link">
+                    CV
+                </a>
+    
+                <a href="portfolio.html" class="nav_link">
+                    Portfolio
+                </a> 
+    
+                <a href="index.html" class="nav_link">
+                    Home
+                </a>
+
+                <a href="base.php" class="nav_link">
+                    base
+                </a>
+    
+            </nav>
+        </div>
+
+        
+
+
+        <div class="row">
+            <div class="column">
+            <h2>Hi, I'm Sydney Humayun!</h2>
+            <p> I have recently changed career paths to Software Engineering and am open to work! </p>
+            <h2> About Me: </h2>
+                <p>
+                    <ul>
+                        <li> Recently finished 2 Code First Girls bootcamps: Software Engineering and Mobile App Development </li>
+                        <li> I really love learning and trying new things! </li>
+                        <li> Worked for ~14 years in education as a teaching assistant across the UK (and a bit in the US)</li>
+                        <li> More info to come! </li>
+                    </ul>
+                </p>
+            </div>
+
+            <div class="column">
+                <p>
+                    <img src="images/Under-construction.png">
+                </p>
+            </div>
+        </div>
+
+        <script src="app.js"></script>
+        <noscript>Please enable JavaScript to view the full site.</noscript>
+
+        
+    </body>
+</html>
