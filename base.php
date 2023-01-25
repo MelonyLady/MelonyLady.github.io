459cc8dcbc1747cba0d7027414a0485fd26f4cf5
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-01-25 15:13:01 +0000

    added php tags and html tag

--- base.php
+++ base.php
@@ -1,7 +1,6 @@
 <?php
-
+session_start();
 ?>
-
 
 
 <!DOCTYPE html>
@@ -16,42 +15,13 @@
     <script src="js/base.js"></script>
 </head>
     <body>
-        
-        <div class="header"> 
+    <!-- end of base -->
+    <?php
+    require_once("header.php");
+    ?>
 
-            <div class="site-identity">
-                <a href="#"><img style="padding: 0px 5px 0px 0px;" src=images/Logo.png 
-                    width="100"
-                    height="100"
-                    alt="Portoflio" /></a>
-            </div>
+    <!-- start of base -->
 
-            <h1>Melony Lady : Sydney Humayun</h1>
 
-            <nav class="topnav">
-            
-                <a href="content.html" class="nav_link">
-                    Content
-                </a>
-    
-                <a href="cv.html" class="nav_link">
-                    CV
-                </a>
-    
-                <a href="portfolio.html" class="nav_link">
-                    Portfolio
-                </a> 
-    
-                <a href="index.html" class="nav_link">
-                    Home
-                </a>
-    
-            </nav>
-        </div>
-
-        <script src="app.js"></script>
-        <noscript>Please enable JavaScript to view the full site.</noscript>
-
-        
     </body>
 </html>
